fd292d1ccb4b36dc7dce011d6d174cdd2e18695b
Emanuele Mazzoni <emanuelemazzoni93@gmail.com>
2016-05-31 15:07:10 +0200

    Added the top navigation bar (it's only missing a proper background) and a WIP homepage

---
+++ devices.php
@@ -0,0 +1,23 @@
+<!-- <?php
+	echo "Hello world!"
+?> -->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title> <!-- INSERT DEVICE NAME HERE -->
+	 	<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css"> <!-- 3.3.6 -->
+
+		<!-- jQuery library -->
+		<script src="js/jquery-1.12.0.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="js/bootstrap.min.js"></script> <!-- 3.3.6 -->
+	</head>
+	<body data-spy="scroll" data-target=".navbar">
+	<?php include 'header.html';?>
+	
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Welcome to TIM!</title>
+	 	<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css"> <!-- 3.3.6 -->
+
+		<!-- jQuery library -->
+		<script src="js/jquery-1.12.0.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="js/bootstrap.min.js"></script> <!-- 3.3.6 -->
+	</head>
+	<body data-spy="scroll" data-target=".navbar">
+	<?php include 'header.html';?>
+	<!-- eliminare da qui -->
+		<div class="container">
+		<div class="jumbotron">
+			<h1>My First Bootstrap Page</h1>
+			<p>Resize this responsive page to see the effect!</p>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+			<h3>Column 1</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+		  </div>
+		  <div class="col-sm-4">
+			<h3>Column 2</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+		  </div>
+		  <div class="col-sm-4">
+			<h3>Column 3</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+		  </div>
+		</div>
+	  </div>
+	  <!-- a qui -->
+	</body>
+</html>
