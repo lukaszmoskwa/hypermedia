a105943bbd8e8a609ba5802602256be359426f31
Emanuele Mazzoni <emanuelemazzoni93@gmail.com>
2016-06-08 16:02:16 +0200

    Added queries to "link" Devices to SL and Assistance, reverse "path" still missing

--- devices.php
+++ devices.php
@@ -9,25 +9,36 @@
 	}
 	//echo "Connected successfully";
 
+	$res = [];
+
 	if (isset($_GET["id"])) { //da mettere in sicurezza!
-		$sql = "SELECT * FROM Devices WHERE ID=" . $_GET["id"]; //da mettere in sicurezza!
+		//$sql = "SELECT * FROM Devices WHERE ID=" . $_GET["id"]; //da mettere in sicurezza! (vecchia query)
+		$sql = "SELECT D.ID, D.Name ImageURL, SL.ID, SL.Name FROM Devices AS D INNER JOIN DevicesSmartLife AS DSL ON D.ID = DSL.IDDevices INNER JOIN SmartLife AS SL ON DSL.IDSmartLife = SL.ID WHERE D.ID=" . $_GET["id"];
+		query($sql);
+		$sql = "SELECT D.ID, D.Name ImageURL, A.ID, A.Name, A.FAQ FROM Devices AS D INNER JOIN DevicesAssistance AS DA ON D.ID = DA.IDDevices INNER JOIN Assistance AS A ON DA.IDAssistance = A.ID WHERE D.ID=" . $_GET["id"];
+		query($sql);
 	} elseif (isset($_GET["category"])) { //da mettere in sicurezza!
 		$sql = "SELECT * FROM Devices WHERE Category=" . $_GET["category"]; //da mettere in sicurezza!
 	}
 	else { //do we use this?
 		$sql = "SELECT * FROM Devices";
 	}
-	$result = $conn->query($sql);
-
-	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while($row = $result->fetch_assoc()) {
-	        $res[] = $row; //adds $row to the $res array
-	    }
-	    echo json_encode($res);
-	} else {
-	    echo "0 results"; //maybe print a better message
+	function query($sql) {
+		global $conn;
+		global $res;
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+		        $res[] = $row; //adds $row to the $res array
+		    }
+		    //echo json_encode($res);
+		} else {
+		    //echo "0 results"; //maybe print a better message
+		}
 	}
+	
+	echo json_encode($res);
 
 	$conn->close();
 ?> 
