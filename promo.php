159a612851f5e4bf2d38bc528fe31c043bb6fc44
Emanuele Mazzoni <emanuelemazzoni93@gmail.com>
2016-06-09 09:26:54 +0200

    Now using query function inside every php file

--- promo.php
+++ promo.php
@@ -1,5 +1,20 @@
 <?php
 	require "db_connect.php"; //defines $servername, $username, $password, $db, $conn (holds the connection)
+
+	function query($sql) {
+		global $conn;
+		global $res;
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+		        $res[] = $row; //adds $row to the $res array
+		    }
+		    //echo json_encode($res);
+		} else {
+		    //echo "0 results"; //maybe print a better message
+		}
+	}
 	
 	//header('Content-type: application/json'); //we encode our result in json
 
@@ -9,32 +24,12 @@
 	}
 	//echo "Connected successfully";
 
+	//$sql = "SELECT * FROM Devices WHERE OldPrice IS NOT NULL UNION SELECT * FROM SmartLife WHERE OldPrice IS NOT NULL"; //SmartLife is missing (maybe also don't use * if we don't need all the data)
+	//query($sql);
 	$sql = "SELECT * FROM Devices WHERE OldPrice IS NOT NULL"; //SmartLife is missing (maybe also don't use * if we don't need all the data)
-	$result = $conn->query($sql);
-
-	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while($row = $result->fetch_assoc()) {
-	        $res[] = $row; //adds $row to the $res array
-	    }
-	    //echo json_encode($res);
-	} else {
-	    //echo "0 results"; //maybe print a better message
-	}
-
-	//$res = []; 
+	query($sql);
 	$sql = "SELECT * FROM SmartLife WHERE OldPrice IS NOT NULL"; //SmartLife is missing (maybe also don't use * if we don't need all the data)
-	$result = $conn->query($sql);
-
-	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while($row = $result->fetch_assoc()) {
-	        $res[] = $row; //adds $row to the $res array
-	    }
-	    //echo json_encode($res);
-	} else {
-	    //echo "0 results"; //maybe print a better message
-	}
+	query($sql);
 
 	echo json_encode($res);
 
--- smartlife.php
+++ smartlife.php
@@ -1,6 +1,21 @@
 <?php
 	require "db_connect.php"; //defines $servername, $username, $password, $db, $conn (holds the connection)
 	
+	function query($sql) {
+		global $conn;
+		global $res;
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+		        $res[] = $row; //adds $row to the $res array
+		    }
+		    //echo json_encode($res);
+		} else {
+		    //echo "0 results"; //maybe print a better message
+		}
+	}
+
 	//header('Content-type: application/json'); //we encode our result in json
 
 	// Check connection
@@ -17,17 +32,10 @@
 	else { //do we use this?
 		$sql = "SELECT * FROM SmartLife";
 	}
-	$result = $conn->query($sql);
 
-	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while($row = $result->fetch_assoc()) {
-	        $res[] = $row; //adds $row to the $res array
-	    }
-	    echo json_encode($res);
-	} else {
-	    echo "0 results"; //maybe print a better message
-	}
+	query($sql);
+
+	echo json_encode($res);
 
 	$conn->close();
 ?> 
