4eecaddc1a23709264c2cbe397e2ef30cacf17ce
Emanuele Mazzoni <emanuelemazzoni93@gmail.com>
2016-06-13 14:21:56 +0200

    Added Device Category in the Path

--- devices.php
+++ devices.php
@@ -28,7 +28,7 @@
 
 	if (isset($_GET["id"])) { //da mettere in sicurezza!
 		//$sql = "SELECT * FROM Devices WHERE ID=" . $_GET["id"]; //da mettere in sicurezza! (vecchia query)
-		$sql = "SELECT D.ID, D.Name, D.ImageURL, TechSpec, D.Description, D.Price, D.OldPrice, SL.ID AS SL_ID, SL.Name AS SL_Name FROM Devices AS D INNER JOIN DevicesSmartLife AS DSL ON D.ID = DSL.IDDevices INNER JOIN SmartLife AS SL ON DSL.IDSmartLife = SL.ID WHERE D.ID=" . $_GET["id"];
+		$sql = "SELECT D.ID, D.Category, D.Name, D.ImageURL, TechSpec, D.Description, D.Price, D.OldPrice, SL.ID AS SL_ID, SL.Name AS SL_Name FROM Devices AS D INNER JOIN DevicesSmartLife AS DSL ON D.ID = DSL.IDDevices INNER JOIN SmartLife AS SL ON DSL.IDSmartLife = SL.ID WHERE D.ID=" . $_GET["id"];
 		query($sql);
 		$sql = "SELECT D.ID, D.Name, D.ImageURL, A.ID AS A_ID, A.Name AS A_NAME, A.FAQ FROM Devices AS D INNER JOIN DevicesAssistance AS DA ON D.ID = DA.IDDevices INNER JOIN Assistance AS A ON DA.IDAssistance = A.ID WHERE D.ID=" . $_GET["id"];
 		query($sql);
--- js/device.js
+++ js/device.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
 			if(status == "success") {
 				result = JSON.parse(data); //we only receive 1 result (result[0])
 				document.title = result[0]["Name"];
+				if (result[0]["Category"] === "Smartphone") {
+					$("#devicecategory").text("> Smartphones");
+					$("#devicecategory").attr("href","devicessmartphones.html");
+				} else if (result[0]["Category"] === "Tablet") {
+					$("#devicecategory").text("> Tablets");
+					$("#devicecategory").attr("href","devicestablets.html");
+				}
 				$("#devicepath").text("> " + result[0]["Name"]);
 				$("#devicepath").attr("href","device.html?id=" + result[0]["ID"]);
 
