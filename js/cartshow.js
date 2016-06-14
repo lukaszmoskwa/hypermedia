3f26fb47241df280d292e17735bc92b100b2cdc5
Emanuele Mazzoni <emanuelemazzoni93@gmail.com>
2016-06-14 18:11:25 +0200

    Better cart list view

--- js/cartshow.js
+++ js/cartshow.js
@@ -1,10 +1,46 @@
 $(document).ready(function(){
 	devices = show("devices");
 	smartlife = show("smartlife");
-	for (i = 0; i < devices.length; i++) {
-		$("#cartdevices").append(devices[i] + "<br>");
-	}
-	for (i = 0; i < smartlife.length; i++) {
-		$("#cartsl").append(smartlife[i] + "<br>");
-	}
+	$.get("devices.php",function(data, status) {
+		result = JSON.parse(data);
+		if(status == "success") {
+			for (j = 0; j < devices.length; j++) {
+				for (i = 0; i < result.length; i++) {
+					if (result[i]["ID"] == devices[j]) {
+						$("#cartdevices").append(
+							"<a href=\"device.html?id=" + result[i]["ID"] + "\">" +
+									"<div class=\"col-md-6\">" + 
+										result[i]["Name"] +
+										"<img class=\"img-responsive\" src=\"images\/" + result[i]["ImageURL"] + "\" alt=\"" + result[i]["Name"] + "\">" +
+									"</div>" +
+								"</a>"
+						)
+						break;
+					}
+				}
+			}
+
+		}
+	});
+	$.get("smartlife.php",function(data, status) {
+		result = JSON.parse(data);
+		if(status == "success") {
+			for (j = 0; j < smartlife.length; j++) {
+				for (i = 0; i < result.length; i++) {
+					if (result[i]["ID"] == smartlife[j]) {
+						$("#cartsl").append(
+							"<a href=\"smartlife.html?id=" + result[i]["ID"] + "\">" +
+									"<div class=\"col-md-6\">" + 
+										result[i]["Name"] +
+										"<img class=\"img-responsive\" src=\"images\/" + result[i]["ImageURL"] + "\" alt=\"" + result[i]["Name"] + "\">" +
+									"</div>" +
+								"</a>"
+						)
+						break;
+					}
+				}
+			}
+
+		}
+	});
 });
