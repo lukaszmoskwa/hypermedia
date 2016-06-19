eddc3517ff2a9fc43732c27b34099047bebbbdad
Emanuele Mazzoni <emanuelemazzoni93@gmail.com>
2016-06-19 15:08:10 +0200

    You can now remove items on the cart directly from the cart page

--- js/cartshow.js
+++ js/cartshow.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 	devices = show("devices");
 	smartlife = show("smartlife");
-	if ((devices.length + smartlife.length) == 0) {
-		$("form").html("<h3>Your cart is empty</h3>");
-	}
+	checkEmpty();
 	$.get("devices.php",function(data, status) {
 		result = JSON.parse(data);
 		if(status == "success") {
@@ -11,12 +9,13 @@ $(document).ready(function(){
 				for (i = 0; i < result.length; i++) {
 					if (result[i]["ID"] == devices[j]) {
 						$("#cartdevices").append(
-							"<a href=\"device.html?id=" + result[i]["ID"] + "\">" +
-									"<div class=\"col-md-6\">" + 
+							"<div class=\"col-md-6\" id=device" + result[i]["ID"] + ">" + 
+								"<a href=\"device.html?id=" + result[i]["ID"] + "\">" +
 										result[i]["Name"] +
-										"<img class=\"img-responsive\" src=\"images\/" + result[i]["ImageURL"] + "\" alt=\"" + result[i]["Name"] + "\">" +
-									"</div>" +
-								"</a>"
+									"<img class=\"img-responsive\" src=\"images\/" + result[i]["ImageURL"] + "\" alt=\"" + result[i]["Name"] + "\">" +
+								"</a>" +
+								"<button class=\"removecartdevice\" id=\"" + result[i]["ID"] + "\">Remove From Cart</button>" +
+							"</div>"
 						)
 						break;
 					}
@@ -32,12 +31,13 @@ $(document).ready(function(){
 				for (i = 0; i < result.length; i++) {
 					if (result[i]["ID"] == smartlife[j]) {
 						$("#cartsl").append(
-							"<a href=\"smartlifesingle.html?id=" + result[i]["ID"] + "\">" +
-									"<div class=\"col-md-6\">" + 
+							"<div class=\"col-md-6\" id=smartlife" + result[i]["ID"] + ">" + 
+								"<a href=\"smartlifesingle.html?id=" + result[i]["ID"] + "\">" +
 										result[i]["Name"] +
-										"<img class=\"img-responsive\" src=\"images\/" + result[i]["ImageURL"] + "\" alt=\"" + result[i]["Name"] + "\">" +
-									"</div>" +
-								"</a>"
+									"<img class=\"img-responsive\" src=\"images\/" + result[i]["ImageURL"] + "\" alt=\"" + result[i]["Name"] + "\">" +
+								"</a>" +
+								"<button class=\"removecartsl\" id=\"" + result[i]["ID"] + "\">Remove From Cart</button>" +
+							"</div>"
 						)
 						break;
 					}
@@ -46,4 +46,25 @@ $(document).ready(function(){
 
 		}
 	});
+	$.getScript("js/cookie.js", function() { //we defined the function to manipulate cookies here
+		$.getScript("js/cart.js", function() { //we defined the function to manipulate the cart here
+			$(".removecartdevice").click(function(event){
+				removeItem("devices",event.target.id);
+				$("#device" + event.target.id).hide();
+				checkEmpty();
+			});
+			$(".removecartsl").click(function(event){
+				removeItem("smartlife",event.target.id);
+				$("#smartlife" + event.target.id).hide();
+				checkEmpty();
+			});
+		});
+	});
 });
+function checkEmpty() {
+	devices = show("devices");
+	smartlife = show("smartlife");
+	if ((devices.length + smartlife.length) == 0) {
+		$("form").html("<h3>Your cart is empty</h3>");
+	}
+}
