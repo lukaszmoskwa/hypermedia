81843ad19ba9889eca5245d3e15bb4e81d79e42e
Emanuele Mazzoni <emanuelemazzoni93@gmail.com>
2016-06-20 21:37:30 +0200

    Smartphones are now shown 3 per row, like in the mockup

--- js/devicessmartphones.js
+++ js/devicessmartphones.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 				if (result[i]["Description"] != null) { //if the link is "clickable"
 					$('#smartphones').append(
 						"<a href=\"device.html?id=" + result[i]["ID"] + "\">" +
-							"<div class=\"col-sm-6\">" + 
+							"<div class=\"col-sm-4\">" + 
 								result[i]["Name"] +
 								"<img class=\"img-responsive\" src=\"images\/" + result[i]["ImageURL"] + "\" alt=\"" + result[i]["Name"] + "\">" +
 							"</div>" +
@@ -14,7 +14,7 @@ $(document).ready(function(){
 					)
 				} else {
 					$('#smartphones').append(
-						"<div class=\"col-sm-6\">" + 
+						"<div class=\"col-sm-4\">" + 
 							result[i]["Name"] +
 							"<img class=\"img-responsive\" src=\"images\/" + result[i]["ImageURL"] + "\" alt=\"" + result[i]["Name"] + "\">" +
 						"</div>"
